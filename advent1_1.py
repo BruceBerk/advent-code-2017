ebfb297f354f7e5d1cab2bf3bea3d6b017dcabb3
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2017-12-01 14:15:53 -0500

    finished 1.1 algorithm code

--- advent1_1.py
+++ advent1_1.py
@@ -6,6 +6,11 @@
 # 12/01/17
 #
 
+testCases = { '1234' : 0,
+              '1122' : 3,
+              '1111' : 4,
+              '91212129' : 9}
+
 def advInput(day):
     """Open the data file for a given day"""
     filename = 'data/input{}.txt'.format(day)
@@ -13,18 +18,30 @@ def advInput(day):
 
 def sumMatchingDigits(str):
     """Process a sequence of digits composing a sum of those that match their neighbor"""
+    print('Summing ', str)
+
     summed = 0
     if len(str) > 0:
         firstDigit = str[0]
+        prevDigit = str[-1]
+        for x in str:
+            if x == prevDigit:
+                summed += int(x)
+            prevDigit = x
 
     return summed
+
+for k, v in testCases.items():
+    ans = sumMatchingDigits(k)
+    print('Got back ', ans)
+    if ans != v:
+        print('   !!! Answer is wrong!')
+    else:
+        print('   *** Correct!')
 
 f = advInput(1)
 s = f.read()
 
-print(s)
-
-teststr = '1122'
-ans = sumMatchingDigits(teststr)
+ans = sumMatchingDigits(s)
 
 print(ans)
