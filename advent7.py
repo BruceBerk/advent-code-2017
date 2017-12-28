2d99b7cef3b32fb4217e4b2f70a1c7f10dbb3478
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2017-12-27 23:19:46 -0500

    Puzzle 7.1 - Recursion Circus completed

--- advent7.py
+++ advent7.py
@@ -63,6 +63,32 @@ program?
 
 Answer 7.1: ykpsek
 
+--- Part Two ---
+
+The programs explain the situation: they can't get down. Rather, they could get down, if they weren't expending all
+of their energy trying to keep the tower balanced. Apparently, one program has the wrong weight, and until it's fixed,
+they're stuck here.
+
+For any program holding a disc, each program standing on that disc forms a sub-tower. Each of those sub-towers are
+supposed to be the same weight, or the disc itself isn't balanced. The weight of a tower is the sum of the weights of
+the programs in that tower.
+
+In the example above, this means that for ugml's disc to be balanced, gyxo, ebii, and jptl must all have the same
+weight, and they do: 61.
+
+However, for tknk to be balanced, each of the programs standing on its disc and all programs above it must each match.
+This means that the following sums must all be the same:
+
+ugml + (gyxo + ebii + jptl) = 68 + (61 + 61 + 61) = 251
+padx + (pbga + havc + qoyq) = 45 + (66 + 66 + 66) = 243
+fwft + (ktlj + cntj + xhth) = 72 + (57 + 57 + 57) = 243
+As you can see, tknk's disc is unbalanced: ugml's stack is heavier than the other two. Even though the nodes above ugml
+are balanced, ugml itself is too heavy: it needs to be 8 units lighter for its stack to weigh 243 and keep the towers
+balanced. If this change were made, its weight would be 60.
+
+Given that exactly one program is the wrong weight, what would its weight need to be to balance the entire tower?
+
+Answer 7.2:
 """
 
 
@@ -134,10 +160,88 @@ def find_root(tower: dict) -> str:
 
     return name
 
+
+def check_for_mismatch(hold_child_totals):
+    """Check a list of ints for an item that does not match
+       Return the amount of discrepancy and the index of the out-of-whack item"""
+
+    ret_vals = None
+    mismatch_amount = 0
+    match_count = [0 for i in range(len(hold_child_totals))]
+
+    #brute force comparison
+    mismatch_found = False
+    for i in range(len(hold_child_totals)-1):
+        for j in range(i+1, len(hold_child_totals)):
+            if hold_child_totals[i] == hold_child_totals[j]:
+                match_count[i] += 1
+                match_count[j] += 1
+            else:
+                mismatch_found = True
+
+
+    if mismatch_found:
+        for i in range(len(match_count)):
+            if match_count[i] == 0:
+                if i == 0:
+                    mismatch_amount = hold_child_totals[1] - hold_child_totals[0]
+                else:
+                    mismatch_amount = hold_child_totals[i-1] - hold_child_totals[i]
+                ret_vals = (i, mismatch_amount)
+                break
+
+    ### need to return the index of the wrong amount as well as the amount to adjust
+    return ret_vals
+
+
+def find_imbalance(tower, node_name):
+    """Visit the sub tower starting at this node and check for an imbalance in the weight
+       Using recursion to solve - Yikes"""
+
+    adjust_weight = 0
+    total_weight = 0
+
+    print('   visiting node', node_name)
+    node = tower[node_name]
+
+    total_weight = node.weight
+    if len(node.children) != 0:
+        # hold the child totals in a list
+        hold_child_totals = []
+        for kid_node in node.children:
+            (kid_total, kid_adjust) = find_imbalance(tower, kid_node)
+            if kid_adjust != 0:
+                return (0, kid_adjust)
+
+            total_weight += kid_total
+            hold_child_totals.append(kid_total)
+
+        print('         these are the kid totals', hold_child_totals)
+        # see if one of the kid totals does not match the others
+        mismatch_vals = check_for_mismatch(hold_child_totals)
+        if mismatch_vals != None:
+            adjust_weight = mismatch_vals[1]
+            child_to_be_adjusted = mismatch_vals[0]
+            bad_kid_name = node.children[child_to_be_adjusted]
+            bad_kid_node = tower[bad_kid_name]
+            adjust_weight = bad_kid_node.weight + mismatch_vals[1]
+
+    print('      node', node_name, 'returning weight', total_weight, 'and', adjust_weight, 'adjustment weight')
+    return (total_weight, adjust_weight)
+
+
 filename = 'data/test7.txt'
 tower = build_tower(filename)
-print('The root is at', find_root(tower))
+root = find_root(tower)
+print('The root is at', root)
+
+weights = find_imbalance(tower, root)
+print('the adjustment weight is', weights[1])
 
 filename = 'data/input7.txt'
 tower = build_tower(filename)
-print('The root is at', find_root(tower))
+root = find_root(tower)
+print('The root is at', root)
+
+weights = find_imbalance(tower, root)
+print('the adjustment weight is', weights[1])
