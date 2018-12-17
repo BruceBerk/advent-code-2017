f1b6fa6d41a580d14e9c1e40dcbe12d1df91fd73
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2018-12-17 12:12:27 -0500

    Updated 15.1 to use a Generator class

--- day15/generators.py
+++ day15/generators.py
@@ -4,6 +4,26 @@ GEN_B_MULTIPLIER = 48271
 
 ITER_COUNT = 40000000
 
+# Part 2 - only send values that are multiples
+A_SEND = 4
+B_SEND = 8
+
+# Part 2 - new iteration count
+PART2_ITER_COUNT = 5000000
+
+
+class Generator:
+    """A class to generate numbers"""
+    def __init__(self, start_v: int, m: int, r: int):
+        self.val = start_v
+        self.multiplier = m
+        self.remainder = r
+
+    def next_val(self) -> int:
+        self.val = (self.val * self.multiplier) % self.remainder
+        return self.val
+
+
 def next_a_val(in_val: int) -> int:
     return (in_val * GEN_A_MULTIPLIER) % MOD_FACTOR
 
@@ -17,16 +37,20 @@ def count_gen_matches(a_val: int, b_val: int) -> int:
        then compare the low 16 bits to see if they match
        Return the number of matching iterations"""
 
+    print('Getting match count...')
     match_count = 0
+
+    gen_a = Generator(a_val, GEN_A_MULTIPLIER, MOD_FACTOR)
+    gen_b = Generator(b_val, GEN_B_MULTIPLIER, MOD_FACTOR)
 
     for idx in range(ITER_COUNT):
         # generate the next values
-        a_val = next_a_val(a_val)
-        b_val = next_b_val(b_val)
+        a_num = gen_a.next_val()
+        b_num = gen_b.next_val()
 
         # get the rightmost 16 bits of each number
-        match_a = a_val % 65536
-        match_b = b_val % 65536
+        match_a = a_num % 65536
+        match_b = b_num % 65536
 
         # if they match, increment the counter
         if match_a == match_b:
@@ -38,12 +62,14 @@ def count_gen_matches(a_val: int, b_val: int) -> int:
 test_a = 65
 test_b = 8921
 
+test_gen_a = Generator(test_a, GEN_A_MULTIPLIER, MOD_FACTOR)
+test_gen_b = Generator(test_b, GEN_B_MULTIPLIER, MOD_FACTOR)
+
 for idx in range(5):
-    test_a = next_a_val(test_a)
+    num_a = test_gen_a.next_val()
+    num_b = test_gen_b.next_val()
 
-    test_b = next_b_val(test_b)
-
-    print(test_a, "   ", test_b)
+    print(num_a, "   ", num_b)
 
 test_match_count = count_gen_matches(test_a, test_b)
 print('Test match count is', test_match_count)
